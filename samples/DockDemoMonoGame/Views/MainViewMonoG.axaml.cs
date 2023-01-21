50b6d25d00e17f1d1337bb512f92a3049a6ad847
SebasOrtiz <sebasortizsanta@gmail.com>
2023-01-21 15:12:26 -0300

    Sebas: [add] Begin new project DockDemoMonoGame

---
+++ samples/DockDemoMonoGame/Views/MainViewMonoG.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DockDemoMonoGame.Views;
+
+public partial class MainViewMonoG : UserControl
+{
+    public MainViewMonoG()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
