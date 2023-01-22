8234850b0dcd118161552283c955a8a50bd04078
eagle1 <damian.hallbauer@gmail.com>
2023-01-21 18:55:50 -0600

    avoid cange conflic on color mabye

--- samples/DockMvvmSample/ViewModels/Tools/Tool1ViewModel.cs
+++ samples/DockMvvmSample/ViewModels/Tools/Tool1ViewModel.cs
@@ -1,7 +1,36 @@
 ﻿using Dock.Model.Mvvm.Controls;
+//using Microsoft.Xna.Framework.Graphics;
+//using Microsoft.Xna.Framework;
 
 namespace DockMvvmSample.ViewModels.Tools;
 
+//test.. see if there is a name conflick.
+using Color = Avalonia.Media.Color;
+
 public class Tool1ViewModel : Tool
 {
+
+
+
+
+
+    public Game1 GameSource => new Game1();
+
+
+
+}
+
+public class Game1 : Microsoft.Xna.Framework.Game
+{
+    protected override void LoadContent()
+    {
+        base.LoadContent();
+    }
+
+    protected override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+    {
+
+        GraphicsDevice.Clear(Microsoft.Xna.Framework.Color.Beige);
+        base.Draw(gameTime);
+    }
 }
