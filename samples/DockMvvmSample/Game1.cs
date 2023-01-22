deb4bc3297e1bca189cbba39cf2c9b8331b8ddf1
eagle1 <damian.hallbauer@gmail.com>
2023-01-21 18:55:07 -0600

    try putting it in view modle ting.. i doonn same issue..
    nseed s the avalonia.monogame built as a project... not a nuget so we cna see why the controcutor of thecontent conrol fails.. its trying to set up the layout but cant construct it...
    i guess..   mabye you tried all this..
    
    im still curious if you could see resutss besides blakce.. for the other windows.. i can see text... and eatin ghte exception its loads the rest...
    
    try a few things worried there is an issue with name conficts around color becaue eits using reflection

---
+++ samples/DockMvvmSample/Game1.cs
@@ -0,0 +1,33 @@
+﻿global using Color = Avalonia.Media.Color;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Microsoft.Xna.Framework;
+//using MonoGame.Framework;
+
+
+
+
+ namespace DockMvvmSample
+{
+
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        public Game1 GameSource => new Game1();
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+        
+        protected override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+
+            GraphicsDevice.Clear(Microsoft.Xna.Framework.Color.Aqua);
+            base.Draw(gameTime);
+        }
+
+    }
+}
--- samples/DockMvvmSample/Views/Tools/Tool1View.axaml.cs
+++ samples/DockMvvmSample/Views/Tools/Tool1View.axaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using System.Diagnostics;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace DockMvvmSample.Views.Tools;
@@ -12,6 +14,29 @@ public class Tool1View : UserControl
 
     private void InitializeComponent()
     {
-        AvaloniaXamlLoader.Load(this);
+        //todo try a put the source for the content control
+        //mg thing of avalonina.inside.. but not his complex game class
+        //i have a simples possible one.. 
+        //AvaloniaInside.MonoGame    the nuget doesnt come with symbols i can pull
+        //do its needs to get a brush out of a theme or whatever.. im guessin backgroud
+
+        //its purplse if thats a clue..  thoug mabyue if sayingits white it wonttry 
+        //themeing it but it is looking for that i guess...
+    
+
+        Background = Avalonia.Media.Brushes.White;
+        try
+        {
+
+
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        catch (Exception ex)
+        {
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
     }
 }
